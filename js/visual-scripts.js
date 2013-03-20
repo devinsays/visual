7a87f7fdfc1c86a3707e21789a48b7f2b36034e1
Devin Price <devin@wptheming.com>
2013-03-19 23:16:32 -0500

    Remove lazy load js.

--- js/visual-scripts.js
+++ js/visual-scripts.js
@@ -1,29 +1,5 @@
 jQuery( document ).ready( function( $ ) {
 
-	// jQuery Plugin for post loading images
-	
-	$.fn.postLoadImages = function(callback) {
-		var imgLen = this.length,
-			count = 0;
-		return this.each(function(count) {
-			count++;
-			if ($(this).attr('data-src')) {
-				var imgTag = this, imgSrc = $(this).attr('data-src');
-				i = new Image();
-				i.onload = function() {
-					$(imgTag).fadeTo(0,0).attr('src',imgSrc).fadeTo(400,1,function() {
-						if (imgLen == count) {
-							if (typeof callback == 'function') {
-								callback.call(this);
-							}
-						}
-					});
-				};
-				i.src = imgSrc;
-			}
-		});
-	};
-
 	// jQuery Plugin for doing a fade/toggle
 	
 	$.fn.slideFadeToggle = function( speed, easing, callback ) {
