d2d042861a2364e806c8aababeab596054f2c371
Devin Price <devin@wptheming.com>
2013-05-24 21:22:30 -0500

    Add fullwidth page template.

---
+++ page-fullwidth.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Full Width, No Sidebar
+ * The template for displaying a page without a sidebar
+ *
+ * @package Visual
+ * @since Visual 0.5
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() )
+						comments_template();
+				?>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
